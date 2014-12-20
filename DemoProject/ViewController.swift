76f93398cf1d9384be6054e257dafc78d9c9db96
Артем Шапет <artyomshapet@MacBook-Pro-Artem.local>
2014-12-20 19:03:18 +0200

    First commit

--- DemoProject/ViewController.swift
+++ DemoProject/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.showsUserLocation = true
+        
+        var location = CLLocationCoordinate2D(latitude: 32.074573, longitude: 34.79181)
+        var span = MKCoordinateSpanMake(0.1, 0.1)
+        var region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        var annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Azrieli Center"
+        annotation.subtitle = "Super Azrieli"
+        //annotation.
+        
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
